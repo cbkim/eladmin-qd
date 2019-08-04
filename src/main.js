cfd93c1c2fdddcff71da90a3ba3ad894e0408e4c
Bobby Kimutai <bkimutai@csquared.com>
2019-08-04 07:44:33 +0300

    added i18n

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+// import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import mavonEditor from 'mavon-editor'
 import 'mavon-editor/dist/css/index.css'
 
@@ -16,6 +17,7 @@ import store from './store'
 
 import '@/icons' // icon
 import './router/index' // permission control
+import i18n from './lang/lang'
 
 Vue.use(mavonEditor)
 Vue.use(permission)
@@ -27,5 +29,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   render: h => h(App)
 })
